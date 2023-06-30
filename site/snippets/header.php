2d1bdfab260898b0d0fbaeadff94cb1bf6a49e7c
eddybabyable <80333067+eddybabyable@users.noreply.github.com>
2023-06-30 14:13:33 +0100

    Add files via upload

---
+++ site/snippets/header.php
@@ -0,0 +1,10 @@
+<div class="header">
+    <div class="logo">
+        <a href="<?= $site->url() ?>">
+            <img id="logo" src="" alt="Logo">
+        </a>
+    </div>
+    <div class="info">
+        <a href="amaInfo.html">Info</a>
+    </div>
+</div>
---
+++ site/templates/projects.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php snippet('meta') ?>
+
+<body>
+    <div class="bodyWrapper">
+        <?php snippet('header') ?>
+        <div class="indexWrapper">
+            <div class="indexLeft">
+                <div class="sliderWrapper">
+                    <section class="splide">
+                        <div class="splide__track">
+                            <ul class="splide__list">
+                                <?php foreach ($page->children() as $project) : ?>
+                                    <li class="splide__slide">
+                                        <img src="<?= $project->image()->url() ?>" alt="">
+                                    </li>
+                                <?php endforeach ?>
+                            </ul>
+                        </div>
+                    </section>
+                </div>
+            </div>
+            <div class="indexRight">
+                <ul>
+                    <?php foreach ($page->children() as $index => $project) : ?>
+                        <li>
+                            <a class="itemLeft" href="<?= $project->url() ?>" data-index="<?= $index ?>"><?= $project->title() ?></a>
+                            <a class="itemRight" href="<?= $project->url() ?>"><?= $project->category() ?></a>
+                        </li>
+                    <?php endforeach ?>
+                </ul>
+            </div>
+        </div>
+        <?php foreach ($page->children() as $project) : ?>
+            <img src="<?= $project->image()->url() ?>" alt="">
+        <?php endforeach ?>
+        <img src="/content/home/projects/Calder Stories/Calder.png" alt="">
+    </div>
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            new Splide('.splide', {
+                autoplay: true,
+                type: 'loop',
+                interval: 2000, // Autoplay interval in milliseconds
+            }).mount();
+        });
+    </script>
+</body>
+
+</html>
